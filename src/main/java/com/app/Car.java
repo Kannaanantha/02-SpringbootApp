602fd544bdf9a44cb405067b2c8f6e8235a7e523
kannaanantha.v <vkannaanantha@enoahisolution.com>
2020-07-06 17:00:14 +0530

    String variable added

--- src/main/java/com/app/Car.java
+++ src/main/java/com/app/Car.java
@@ -7,9 +7,18 @@ public class Car {
 	 * Kanna anantha 
 	 */
 	private int a;
+	private String b;
 
 	public int getA() {
 		return a;
+	}
+
+	public String getB() {
+		return b;
+	}
+
+	public void setB(String b) {
+		this.b = b;
 	}
 
 	public void setA(int a) {
