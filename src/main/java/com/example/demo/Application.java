7043da3e7265606075f5fe578e10d72efeb631d2
kannaanantha.v <vkannaanantha@enoahisolution.com>
2020-07-06 18:26:12 +0530

    HIS-134 fix provided

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -11,7 +11,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
-		
+		//chages for HIS-134
 		run.close();
 		
 	
